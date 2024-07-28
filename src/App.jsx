c144562dd369c371b898a00b36c22fed93ed07c6
Mainul Islam <maainul.islam@gmail.com>
2024-07-28 14:11:13 +0600

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Navbar from './Components/Navbar'
+
+function App() {
+ 
+  return (
+    <>
+      <Navbar/>
+      <div className="container">
+        <div className="bg-red-600">
+        Hello world!
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      hey i am a navbar
+    </div>
+  )
+}
+
+export default Navbar
